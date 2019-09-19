3a5eca8e35b451614b82d874c1f2cd62b25b02b9
Xavir <xaviranik.me@gmail.com>
2019-09-19 09:56:19 +0600

    Added: User, Admin migration

--- database/migrations/2019_09_19_015329_create_admins_table.php
+++ database/migrations/2019_09_19_015329_create_admins_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateAdminsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,15 +12,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('admins', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('type');
-            $table->tinyInteger('activation')->default(0);
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
-            $table->timestamp('email_verified_at')->nullable();
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('admins');
     }
 }
