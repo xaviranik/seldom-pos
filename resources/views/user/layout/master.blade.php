7ed35576ac00df9660a5423836cb74b4fef59c5d
Xavir <xaviranik.me@gmail.com>
2019-09-21 11:17:30 +0600

    Added: datatable bootstrap to app.scss. Fixed: Load time

--- resources/views/user/layout/master.blade.php
+++ resources/views/user/layout/master.blade.php
@@ -9,9 +9,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/w/bs4/dt-1.10.18/datatables.min.css" />
+    <title>{{ config('app.name', 'Seldom-POS') }}</title>
 
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
